388e23cf8dd1246664967fb918ef64396f1c6f48
Tim <50641103+tikhusaenov@users.noreply.github.com>
2020-12-06 13:44:26 +0300

    socketManager created

--- server/index.js
+++ server/index.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
-
-const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
-
 const router = require('./router')
 const app = express()
 const server = http.createServer(app)
@@ -15,50 +12,7 @@ const io = socketio(server, {
         credentials: true
     }
 })
-
-io.on('connection', socket => {
-    console.log('We have a new connection!!!')
-
-    socket.on('join', ({ name }, callback) => {
-        console.log(name)
-        const { error, user } = addUser({ id: socket.id, name})
-
-        if (error) return callback(error)
-
-        socket.emit('message', {user: 'admin', text: `${user.name}, welcome to the chat room!`})
-        socket.broadcast.emit('message', {user: 'admin', text: `${user.name} has joined!`})
-        io.emit('roomData', {  users: getUsersInRoom() });
-        callback()
-    })
-
-    socket.on('sendMessage', (message, callback) => {
-        const user = getUser(socket.id)
-        console.log(user)
-        if (user) {
-            io.emit('message', { user: user.name, text: message})
-        }
-        callback()
-    })
-
-
-
-    socket.on('typing', () => {
-        const user = getUser(socket.id)
-        socket.broadcast.emit('myTyping', {user: user.name})
-
-
-    })
-
-    socket.on('disconnect', () => {
-        console.log('User had left!!!')
-        const user = removeUser(socket.id);
-        if(user) {
-            io.emit('message', { user: 'admin', text: `${user.name} has left.` });
-            io.emit('roomData', { users: getUsersInRoom()});
-        }
-    })
-})
-
 app.use(router)
-
 server.listen(process.env.PORT || 5000, () => console.log(`Server has started.`));
+
+module.exports = io
