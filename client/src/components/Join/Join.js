4871137be5de974e7bce3226b0e6a4313be330c2
Tim <50641103+tikhusaenov@users.noreply.github.com>
2020-12-27 17:35:20 +0300

    styled app

--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -12,11 +12,15 @@ export default function SignIn() {
             <div className="joinInnerContainer">
                 <h1 className="heading">Join the chat</h1>
                 <div>
-                    <input placeholder="Name" className="joinInput" type="text" onChange={(event) => setName(event.target.value)} />
+                    <input placeholder="Type your name..."
+                           className="joinInput"
+                           type="text"
+                           onChange={(event) => setName(event.target.value)}
+                    />
                 </div>
 
                 <Link onClick={e => (!name) ? e.preventDefault() : null} to={`/chat?name=${name}`}>
-                    <button className={'button mt-20'} type="submit">Sign In</button>
+                    <button className='buttonSubmit mt-20' type="submit">Sign In</button>
                 </Link>
             </div>
         </div>
